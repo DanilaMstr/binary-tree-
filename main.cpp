2a5e5a677b56e14d1d0820e94cca82521636b2ad
Danila Ponukaev <danila_ponukaev@mail.ru>
2019-09-23 20:02:58 +0300

    first version

---
+++ main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+
+    int    iData; //key
+    double dData;
+    Node* leftChild;
+    Node* rightChild;
+
+    void displayNode()
+    {
+        cout << "{ " << iData  << "," <<  dData << "}" << endl;
+    }
+
+};
+
+class Tree
+{
+private:
+    Node* root;
+
+public:
+    Tree()
+    {
+        root = nullptr;
+    }
+
+    Node* finde(int key)
+    {
+        Node* current = root;
+        while(current->iData != key)
+        {
+            if (current->iData < key)
+                current = current->leftChild;
+            else
+                current = current->rightChild;
+            if (current == nullptr)
+                return nullptr;
+        }
+        return current;
+    }
+
+    void insert(int key ,double dData)
+    {
+        Node* newNode = new Node;
+        newNode->iData = key;
+        newNode->dData = dData;
+        if (root == nullptr)
+        {
+            root = newNode;
+            return;
+        }
+        else
+        {
+            Node* current;
+            Node* parent;
+            current = root;
+            while (true)
+            {
+                parent = current;
+                if (key < current->iData)
+                {
+                    current = current->leftChild;
+                    if (current == nullptr)
+                    {
+                        parent->leftChild = newNode;
+                        return;
+                    }
+                }
+                else
+                {
+                    current = current->rightChild;
+                    if (current == nullptr)
+                    {
+                        parent->rightChild = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    
+
+
+
+
+
+
+};
+
+int main() {
+
+
+    return 0;
+}
